6a6dfdaf8c3efa705bbfa2a75d67ea24fb7f6005
Mustafa Al <146637002+codriot@users.noreply.github.com>
2024-11-13 17:45:26 +0300

    first commit

---
+++ src/main/java/com/MustCloud/Cloud/MustCloudApplication.java
@@ -0,0 +1,13 @@
+package com.MustCloud.Cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustCloudApplication.class, args);
+	}
+
+}
